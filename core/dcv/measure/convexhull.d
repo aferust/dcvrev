26a6e50da0cfbb043607173092961a89098e32d1
aferust <aferust@gmail.com>
2021-12-15 16:00:00 +0300

    fix wspace

--- core/dcv/measure/convexhull.d
+++ core/dcv/measure/convexhull.d
@@ -41,31 +41,31 @@ auto convexHull(alias indices_only = true, PTSlice)(PTSlice points) @nogc nothro
     auto p = _p[].ssort;
     
     auto upper_pts = scopedBuffer!Coord;
-	upper_pts.put(p[0]); upper_pts.put(p[1]);
-	
+    upper_pts.put(p[0]); upper_pts.put(p[1]);
+    
     foreach (i; 2..n){
-		upper_pts.put(p[i]);
-		while ((upper_pts.length > 2) && 
+        upper_pts.put(p[i]);
+        while ((upper_pts.length > 2) && 
             !rightTurn(upper_pts.data[$ - 1], upper_pts.data[$ - 2], upper_pts.data[$ - 3])){
-			Coord tmp = upper_pts.data[$-1];
+            Coord tmp = upper_pts.data[$-1];
             upper_pts.popBackN(2);
             upper_pts.put(tmp);
         }
     }
     
-	auto lower_pts = scopedBuffer!Coord;
+    auto lower_pts = scopedBuffer!Coord;
     lower_pts.put(p[$ - 1]); lower_pts.put(p[$ - 2]);
-	
+    
     foreach_reverse (i; 0..(n - 2)){
-		lower_pts.put(p[i]);
-		while ((lower_pts.length > 2) && !rightTurn(lower_pts.data[$ - 1], lower_pts.data[$ - 2], lower_pts.data[$ - 3])){
-			Coord tmp = lower_pts.data[$-1];
+        lower_pts.put(p[i]);
+        while ((lower_pts.length > 2) && !rightTurn(lower_pts.data[$ - 1], lower_pts.data[$ - 2], lower_pts.data[$ - 3])){
+            Coord tmp = lower_pts.data[$-1];
             lower_pts.popBackN(2);
             lower_pts.put(tmp);
         }
     }
     
-	auto lower = lower_pts.data[1 .. $-1];
+    auto lower = lower_pts.data[1 .. $-1];
 
     static if(indices_only){
         RCArray!size_t hull_indices = RCArray!size_t(lower.length + upper_pts.length); // returning indices are of size_t
@@ -88,7 +88,7 @@ auto convexHull(alias indices_only = true, PTSlice)(PTSlice points) @nogc nothro
 
 pragma(inline, true)
 private bool rightTurn(PC)(PC p1, PC p2, PC p3) {
-	if ((p3.y-p1.y)*(p2.x-p1.x) >= (p2.y-p1.y)*(p3.x-p1.x))
+    if ((p3.y-p1.y)*(p2.x-p1.x) >= (p2.y-p1.y)*(p3.x-p1.x))
         return false;
-	return true;
-}	
+    return true;
+}    
