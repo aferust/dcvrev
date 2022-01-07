e633538ba48b9f0bcb9e0c519f56df11ea96e1a3
aferust <aferust@gmail.com>
2022-01-07 16:41:51 +0300

    tidy

--- core/dcv/morphology/distancetransform.d
+++ core/dcv/morphology/distancetransform.d
@@ -8,7 +8,7 @@ import mir.rc;
 /** Apply distance transform using chamfer distance algorithm.
 
 Params:
-    image = Input binary image of ubyte (0 for background). Agnostic to SliceKind
+    image = Input binary image (0 for background). Agnostic to SliceKind
 
 Returns distance image of Slice!(RCI!int, 2LU, Contiguous)
 */
