38f494fcbdc468fde521ca28a74c0b8028a43ae2
aferust <aferust@gmail.com>
2022-01-10 13:03:38 +0300

    clean labelizer

--- core/dcv/measure/label.d
+++ core/dcv/measure/label.d
@@ -7,20 +7,9 @@ License: $(LINK3 http://www.boost.org/LICENSE_1_0.txt, Boost Software License - 
 module dcv.measure.label;
 
 import std.typecons: tuple, Tuple;
-import std.experimental.allocator.gc_allocator;
-import std.array : Appender;
 
 import mir.ndslice;
 import mir.ndslice.allocation;
-
-/+ // keep this here for now. TODO: convex hull and ellipse fitting
-private alias Point = Tuple!(ulong, "x", ulong, "y");
-
-struct XYList {
-    Appender!(ulong[]) xs;
-    Appender!(ulong[]) ys;
-}
-+/
 
 /**
 Label connected components in 2-D binary image
@@ -31,23 +20,14 @@ Params:
 
 @nogc nothrow:
 
-auto bwlabel(alias Conn = 8, alias Method = 3, SliceKind kind, Iterator)(auto ref Slice!(Iterator, 2LU, kind) input)
+auto bwlabel(alias Conn = 8, SliceKind kind, Iterator)(auto ref Slice!(Iterator, 2LU, kind) input)
 in
 {
     assert(Conn == 4 || Conn == 8, "Connection rule must be either of 4 or 8");
-    assert(Method == 1 || Method == 2 || Method == 3, "Method must be one of 1, 2, and 3");
 }
 do
 {
-    static if(Method == 1){
-        LabelizerV1!(typeof(input)) lblzr;
-    }else
-    static if(Method == 2){
-        LabelizerV2!(typeof(input)) lblzr;
-    } else
-    static if(Method == 3){ // light speed :)
-        LabelizerV3 lblzr;
-    }
+    Labelizer2D lblzr;
     return lblzr.labelize!(Conn)(input);
 }
 
@@ -91,173 +71,11 @@ auto label2rgb(InputType)(InputType label)
     return img;
 }
 
-private struct LabelizerV1(SliceT)
-{
-    Slice!(ulong*, 2LU, SliceKind.contiguous) label;
-
-    size_t row_count;
-    size_t col_count;
-
-    static immutable byte[4] dx4 = [1, 0, -1,  0];
-    static immutable byte[4] dy4 = [0, 1,  0, -1];
-
-    static immutable byte[8] dx8 = [1, -1, 1, 0, -1,  1,  0, -1];
-    static immutable byte[8] dy8 = [0,  0, 1, 1,  1, -1, -1, -1];
-
-    private void dfs(alias Conn)(immutable ulong i, immutable ulong j, immutable ulong current_label, const ref SliceT img)
-    {
-        import std.container : SList;
-        import std.range;
-
-        SList!ulong stack;
-
-        stack.insertFront(i);
-        stack.insertFront(j);
-        
-        while(!stack.empty){
-
-            immutable ulong arg_j = stack[].front; stack.removeFront();
-            immutable ulong arg_i = stack[].front; stack.removeFront();
-
-            if (arg_i< 0 || arg_i == row_count) return; // out of bounds
-            if (arg_j < 0 || arg_j == col_count) return; // out of bounds
-            if (label[arg_i][arg_j] || !img[arg_i][arg_j]) continue; // already labeled or not marked with a number in input
-
-            label[arg_i][arg_j] = current_label;
-            
-            import std.format;
-
-            foreach(direction; 0..Conn){
-                mixin("auto ni = arg_i + " ~ format("dx%d[direction];", Conn));
-                mixin("auto nj = arg_j + " ~ format("dy%d[direction];", Conn));
-
-                if (label[ni][nj] || !img[ni][nj]) continue;
-                stack.insertFront(ni);
-                stack.insertFront(nj);
-            }
-        }
-
-        stack.clear();
-    }
-
-    auto labelize(alias Conn, SliceKind kind)(in Slice!(ubyte*, 2LU, kind) img)
-    {
-        /* The algorithm is based on:
-        * https://stackoverflow.com/questions/14465297/connected-component-labelling
-        */
-        
-        row_count = img.shape[0];
-        col_count = img.shape[1];
-        
-        label = makeUninitSlice!ulong(GCAllocator.instance, img.shape[0..2]);
-        
-        ulong component = 0;
-        foreach (i; 0..row_count) 
-            foreach (j; 0..col_count)
-                if (!label[i][j] && img[i][j])
-                    dfs!(Conn)(i, j, ++component, img);
-        
-        return label;     
-    }
-}
-
-// WARNING !!! Output labels are not sequential with this method
-private struct LabelizerV2(SliceT)
-{
-    SliceT input;
-    
-    size_t w, h;
-    ulong[] component;
-
-    auto labelize(alias Conn, SliceKind kind)(ref Slice!(ubyte*, 2LU, kind) input)
-    {   
-        // gives coordinates of connected components
-        /+XYList[] coords;+/
-        this.input = input;
-        w = input.shape[0];
-        h = input.shape[1];
-
-        auto label = makeUninitSlice!ulong(GCAllocator.instance, input.shape[0..2]);
-        
-        import core.stdc.stdlib;
-        component = cast(ulong[])malloc(w*h * ulong.sizeof)[0..w*h * ulong.sizeof];
-        scope(exit) free(cast(void*)component.ptr);
-        
-        foreach (i; 0..w*h)
-            component[i] = i;
-        foreach (x; 0..w)
-            foreach (y; 0..h)
-                conn!(Conn)(x, y);
-        
-        /+XYList[ulong] pmap;+/
-        
-        foreach (x; 0..w)
-        {
-            foreach (y; 0..h)
-            {
-                if (input[x][y] == 0)
-                {
-                    continue;
-                }
-                ulong c = x*h + y;
-                while (component[c] != c) c = component[c];
-                /+
-                if(c !in pmap)
-                {
-                    XYList tmp;
-                    tmp.xs.reserve(10);
-                    tmp.ys.reserve(10);
-                    tmp.xs ~= x;
-                    tmp.ys ~= y;
-                    pmap[c] = tmp;
-                }else{
-                    pmap[c].xs ~= x;
-                    pmap[c].ys ~= y;
-                }
-                +/
-                label[x][y] = c;
-            }
-        }
-        
-        return label;
-    }
-
-    private:
-
-    void doUnion(ulong a, ulong b) @nogc nothrow
-    {
-        while (component[a] != a)
-            a = component[a];
-        while (component[b] != b)
-            b = component[b];
-        component[b] = a;
-    }
-
-    void unionCoords(ulong x, ulong y, ulong x2, ulong y2) @nogc nothrow
-    {
-        if (y2 < h && x2 < w && input[x][y] && input[x2][y2])
-            doUnion(x*h + y, x2*h + y2);
-    }
-
-    void conn(alias Conn)(ulong x, ulong y) @nogc nothrow
-    {
-        static if(Conn == 4){
-            unionCoords(x, y, x+1, y);
-            unionCoords(x, y, x, y+1);
-        }else
-        static if(Conn == 8){
-            unionCoords(x, y, x+1, y);
-            unionCoords(x, y, x, y+1);
-            unionCoords(x, y, x+1, y+1);
-        }
-    }
-}
-
 /+ a rewrite of https://github.com/scikit-image/scikit-image/blob/main/skimage/measure/_ccomp.pyx
 
    Fiorio, C., & Gustedt, J. (1996). Two linear time union-find strategies for image processing. Theoretical Computer Science, 154(2), 165-181.
 +/
-struct LabelizerV3{
+struct Labelizer2D{
     auto labelize(alias Conn, Iterator, SliceKind kind)(ref Slice!(Iterator, 2LU, kind) input){
         static if(Conn == 4){
            enum _conn = 1;
